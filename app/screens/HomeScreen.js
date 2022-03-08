141f5b2f28afbc8b21ce8ee85c1fb2c35a7482e8
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-03-08 16:48:48 +0100

    'test

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StackNavigator } from "react-navigation";
 
-export default function HomeScreen() {
+function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
