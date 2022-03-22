0497bc6bba264bf2d312f0ad9fde45378f1fa2c5
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-03-22 10:06:31 +0100

    Test

--- app/screens/GetStudents.js
+++ app/screens/GetStudents.js
@@ -1,9 +1,18 @@
 import { View, Text, Button } from "react-native";
-improt
+import axios from "axios"
+
 
 function GetStudents({route, navigation}) {
   const { id } = route.params;
+
+  // const test = axios.crreate({baseURL:'http://localhost:8081/'})
   
+  
+  // test.get('/userManagement/users/student').then(res => {
+  //   console.log(res.data)
+  //   this.setState({students: res.data})
+  // })
+
   return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>id: {JSON.stringify(id)}</Text>
